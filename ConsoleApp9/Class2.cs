f95044c422527dc05969df15729836904d2b28e7
Patan Moulali <patan.moulali@mphasis.com>
2022-08-29 21:46:45 +0530

    Add project files.

---
+++ ConsoleApp9/Class2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Class2
+    {
+        static void Main()
+        {
+            LinearQueue Q = new LinearQueue(5);
+
+            Q.insert(10);
+            Q.insert(20);
+            Q.insert(30);
+            Q.insert(40);
+            Q.insert(50);
+
+            Console.WriteLine("Items are : ");
+            Q.printQueue();
+
+            Q.delete();
+            Q.delete();
+
+            Console.WriteLine("Items are : ");
+            Q.printQueue();
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class LinearQueue
+    {
+        private int[] ele;
+        private int front;
+        private int rear;
+        private int max;
+
+        public LinearQueue(int size)
+        {
+            ele = new int[size];
+            front = 0;
+            rear = -1;
+            max = size;
+        }
+
+        public void insert(int item)
+        {
+            if (rear == max - 1)
+            {
+                Console.WriteLine("Queue Overflow");
+                return;
+            }
+            else
+            {
+                ele[++rear] = item;
+            }
+        }
+
+        public int delete()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Queue is Empty");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("deleted element is: " + ele[front]);
+                return ele[front++];
+            }
+        }
+
+        public void printQueue()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            else
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.WriteLine("Item[" + (i + 1) + "]: " + ele[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+   
